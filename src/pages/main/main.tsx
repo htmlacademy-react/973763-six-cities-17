fbd3fc85b491625a8a2ca8fd594a040272b15425
frenby52 <aakytin@mail.ru>
2025-01-24 01:38:03 +0300

    ref main main-container

--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -1,19 +1,14 @@
-import CardList from '../../components/card-list/card-list';
 import LocationsList from '../../components/locations-list/locations-list';
+import MainContainer from '../../components/main-container/main-container';
 import Header from '../../components/header/header';
-import Map from '../../components/map/map';
-import Sort from '../../components/sort/sort';
-import {CardType, CITIES_NAMES} from '../../const';
-import {useState} from 'react';
+import {CITIES_NAMES} from '../../const';
 import {useAppSelector} from '../../store/use-app-selector';
 import {getActiveCityName, getSortedOffers} from '../../store/selectors';
 
 function Main(): JSX.Element {
-
   const activeCityName = useAppSelector(getActiveCityName);
   const sortedOffers = useAppSelector(getSortedOffers);
   const hasOfferData = sortedOffers.length > 0;
-  const [activeOfferId, setActiveOfferId] = useState<string | null>(null);
 
   return (
     <div className="page page--gray page--main">
@@ -25,32 +20,7 @@ function Main(): JSX.Element {
             <LocationsList IsFavorites={false} cities={CITIES_NAMES} activeCityName={activeCityName}/>
           </section>
         </div>
-        <div className="cities">
-          {hasOfferData ?
-            <div className="cities__places-container container">
-              <section className="cities__places places">
-                <h2 className="visually-hidden">Places</h2>
-                <b className="places__found">{sortedOffers.length} places to stay in {activeCityName}</b>
-                <Sort/>
-                <div className="cities__places-list places__list tabs__content">
-                  <CardList cards={sortedOffers} isFavorites={false} cardType={CardType.Cities} onActiveOfferChange={setActiveOfferId}/>
-                </div>
-              </section>
-              <div className="cities__right-section">
-                <Map type={'cities'} activeOfferId={activeOfferId} offers={sortedOffers} />
-              </div>
-            </div>
-            :
-            <div className="cities__places-container cities__places-container--empty container">
-              <section className="cities__no-places">
-                <div className="cities__status-wrapper tabs__content">
-                  <b className="cities__status">No places to stay available</b>
-                  <p className="cities__status-description">We could not find any property available at the moment in {activeCityName}</p>
-                </div>
-              </section>
-              <div className="cities__right-section"></div>
-            </div>}
-        </div>
+        <MainContainer offers={sortedOffers} activeCityName={activeCityName}/>
       </main>
     </div>
   );
